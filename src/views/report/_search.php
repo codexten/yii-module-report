4635ca1d5c6235b9384b95e2170f638aefe84709
Jomon Johnson <jomon.entero@gmail.com>
2019-07-22 13:21:06 +0530

    init

--- src/views/report/_search.php
+++ src/views/report/_search.php
@@ -1,15 +1,10 @@
 <?php
 
 use codexten\yii\modules\report\reports\ReportInterface;
-use yii\bootstrap\ActiveForm;
 use yii\web\View;
 
 /* @var $this View */
-/* @var $searchModel ReportInterface */
+/* @var $model ReportInterface */
 ?>
 
-<?php $form = ActiveForm::begin() ?>
-
-<?= $this->render('search/_form', ['form' => $form, 'model' => $searchModel]) ?>
-
-<?php ActiveForm::end() ?>
+<?= $this->render('search/_form', ['form' => $form, 'model' => $model]) ?>
--- src/views/report/search/_form.php
+++ src/views/report/search/_form.php
@@ -1,1 +1,14 @@
 <?php
+
+use yii\bootstrap\ActiveForm;
+use yii\web\View;
+
+/* @var $this View */
+?>
+
+<?php $form = ActiveForm::begin() ?>
+
+<?= $this->render('_fields', ['from' => $form, 'model' => $model]) ?>
+
+<?php ActiveForm::end() ?>
+
