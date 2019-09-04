0f253997c4c0eb3858ea2989dd494e8a9c07f252
Jomon Johnson <jomon.entero@gmail.com>
2019-09-04 14:32:41 +0530

    Grid Summary

--- src/grids/ExportMenuTrait.php
+++ src/grids/ExportMenuTrait.php
@@ -2,8 +2,41 @@
 
 namespace codexten\yii\modules\report\grids;
 
+use PhpOffice\PhpSpreadsheet\Cell\Coordinate;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
+
 trait ExportMenuTrait
 {
     use ReportFooterTrait;
+
+    public function init()
+    {
+        $this->onRenderSheet = function (Worksheet $sheet, self $widget) {
+            $lastRow = $this->dataProvider->getTotalCount();
+            $footerRows = $this->getFooterRowsContent();
+
+            $currentRow = $lastRow + 4;
+
+            $sheet->setCellValue("B{$currentRow}", 'Summary');
+            $currentRow++;
+
+            foreach ($footerRows as $footerRow) {
+                $currentCol = 1;
+                foreach ($footerRow as $cell) {
+                    $sheet->setCellValue($this->getCellCoordinateFromIndex($currentRow, $currentCol), $cell);
+                    $currentCol++;
+                }
+                $currentRow++;
+            }
+
+        };
+        parent::init();;
+    }
+
+    public function getCellCoordinateFromIndex($row, $col)
+    {
+        $col = Coordinate::stringFromColumnIndex($col);
+        return "{$col}{$row}";
+    }
 
 }
